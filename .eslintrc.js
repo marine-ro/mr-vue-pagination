85e7aaf94312284071cb8374ecbec43569548bfc
Marine Ro <galmarpet@gmail.com>
2021-07-21 14:52:44 +0400

    feat: init repository

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/airbnb',
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  rules: {
+    'import/no-named-as-default': 0,
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'consistent-return': 'off',
+  },
+  settings: {
+    node: {
+      paths: ['@'],
+      extensions: ['.js', '.vue', '.scss'],
+    },
+  },
+};
---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="page-pagination">
+    <MrPagination />
+  </div>
+</template>
+
+<script>
+import MrPagination from './components/MrPagination.vue';
+
+export default {
+  name: 'App',
+  components: {
+    MrPagination,
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
